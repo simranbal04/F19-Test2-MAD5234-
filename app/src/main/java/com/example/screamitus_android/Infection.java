6af5ed9e7a0b862570b9a5b824af29d641d4479f
simran bal <simranbal01@gmail.com>
2019-09-19 12:02:24 -0400

    RED -r1

--- app/src/main/java/com/example/screamitus_android/Infection.java
+++ app/src/main/java/com/example/screamitus_android/Infection.java
@@ -1,5 +1,31 @@
 package com.example.screamitus_android;
 
-public class Infection {
+import android.app.Activity;
+import android.app.Activity;
+import android.support.v4.widget.TextViewCompat;
+import android.widget.EditText;
 
+import org.junit.Before;
+import org.junit.Test;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
+
+
+public class Infection {
+MainActivity mainActivity;
+    @Before
+    public void setup() throws Exception {
+        mainActivity = Roboelectric.buildActivity(MainActivity.class)
+                .create()
+                .resume()
+                .get();
+    }
+    //r1
+    @Test
+    public void DaysbiggerthanZero()
+    {
+        EditText days = (EditText)mainActivity.findViewById(R.id.daysTextBox);
+        String noofDays = days.getText().toString();
+
+    }
 }
